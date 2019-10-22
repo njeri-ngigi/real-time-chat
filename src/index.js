9f76e9663ca400faa11e74fa76823c7ef2238071
Njeri Ngigi <njery.ngigi@gmail.com>
2019-10-22 19:29:32 +0200

    - update socket to send data to clients

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
-const io = require('socket.io');
+const socketIO = require('socket.io');
 const ENV = require('./environment');
 const router = require('./routes');
 const connectDb = require('./models/database');
@@ -15,17 +17,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/v1', router);
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 connectDb();
 
-const server = app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App running on port ${PORT}`);
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('send message', (data) => {
+    io.sockets.emit('send message', data);
+  });
+  socket.on('disconnect', () => console.log('User disconnected'));
 });
 
-const ioConnection = io(server);
-
-ioConnection.on('connection', (socket) => {
-  // TODO: connect from the client side
-  // eslint-disable-next-line no-console
-  console.log('New user connected');
+server.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
